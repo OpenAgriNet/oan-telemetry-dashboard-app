3320f0cd9865e8a119e0295e2f6434002539c0fb
shashank-kenpath <104070563+shashank-kenpath@users.noreply.github.com>
2025-05-26 17:27:15 +0530

    total questions removed

--- src/pages/SessionsReport.tsx
+++ src/pages/SessionsReport.tsx
@@ -271,7 +271,7 @@ const SessionsReport = () => {
           </CardContent>
         </Card>
 
-        <Card>
+        {/*<Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total Questions</CardTitle>
             <MessageSquare className="h-4 w-4 text-muted-foreground" />
@@ -291,7 +291,7 @@ const SessionsReport = () => {
               }
             </p>
           </CardContent>
-        </Card>
+        </Card>*/}
 
         {/* <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
