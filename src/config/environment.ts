b6410026827e7586cb017047a5ecbace9b6e65f6
anjalirao-b <anjali.rao@kenpath.io>
2025-05-28 15:49:51 +0530

    environment url

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,6 +4,6 @@
 
 // API Configuration
 export const API_CONFIG = {
-  //SERVER_URL: 'https://proddashbaordvistaar.mahapocra.gov.in/v1',
-  SERVER_URL: 'http://localhost:4000/v1'
+  SERVER_URL: 'https://proddashbaordvistaar.mahapocra.gov.in/v1',
+ // SERVER_URL: 'http://localhost:4000/v1'
 };
